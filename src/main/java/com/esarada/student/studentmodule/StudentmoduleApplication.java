a06b3b9dac625501e47455b5061e2147f4b68b71
srinivas <srinivas>
2023-11-04 19:23:04 +0530

    intial commit

---
+++ src/main/java/com/esarada/student/studentmodule/StudentmoduleApplication.java
@@ -0,0 +1,13 @@
+package com.esarada.student.studentmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentmoduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentmoduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esarada/student/studentmodule/StudentmoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esarada.student.studentmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentmoduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
